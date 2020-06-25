959e6284dcb067e1f2fdda23dbedfb87d531d2ba
Brogan Neagle <broganneagle12@gmail.com>
2020-06-25 16:06:14 -0500

    1st commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// display current date 
+var CurrentDay = moment().format();
+
+// local storage for text input
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        console.log('about to update', key, 'with this from local S', value);
+        $(`#text${key}`).text(value);
+    }
+}
+// function for currentday
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    console.log('current time hour!!', moment().hours());
+    var currentHour = moment().hours();
+    for (let i = 9; i < 18; i++) {
+        // row
+        var row = $(`<div data-time=${i} id='${i}' class="row time-block"></div>`);
+
+        // column
+        var c1 = $('<div class="col-sm-2 hour">' + i + 'AM' + '</div>');
+
+        if (i > 12) {
+            var newDisplay = i - 12;
+            newDisplay += 'PM';
+            c1.text(newDisplay);
+        }
+
+        row.append(c1);
+
+        // column 2
+        var c2 = $(`<textarea id=text${i} class="description col-sm-8" ></textarea>`);
+        if (i == currentHour) {
+            c2.addClass("present");
+        } else if (currentHour > i) {
+            c2.addClass("past");
+        } else if (currentHour < i) {
+            c2.addClass('future')
+        }
+        row.append(c2);
+
+        // column 3
+        var c3 = $(`<button id=${i}  class="saveBtn col-sm-2 fas fa-save"></button>`);
+
+       row.append(c3);
+
+        // last step add rows to container
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
+    })
